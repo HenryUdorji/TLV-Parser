1b1bceb3a12008e2c35672126dd7ee80f9b25a4f
ifechukwu.udorji <ifechukwu.udorji@globalacelerex.com>
2024-08-14 12:23:27 +0100

    setup - init

---
+++ app/src/main/java/com/ifechukwu/tlvparser/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.ifechukwu.tlvparser
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.ifechukwu.tlvparser.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
